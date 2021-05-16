b6ecdac3217397eed7c578df256ce706b6fe7806
RapRy <bigbosstitle@gmail.com>
2021-05-15 22:57:04 -0500

    update screenshots

--- src/components/Preview/Screenshots.js
+++ src/components/Preview/Screenshots.js
@@ -1,14 +1,27 @@
+import { useRef, useEffect, useState } from 'react'
+
 const Screenshots = ({ images }) => {
+    const screenshotsContainer = useRef()
+    const [scrollWidth, set] = useState(0)
+
+    const scroll = (e) => {
+        screenshotsContainer.current.scrollLeft = e.target.value
+    }
+
+    useEffect(() => {
+        screenshotsContainer.current !== undefined && set(screenshotsContainer.current.scrollWidth - screenshotsContainer.current.clientWidth)
+    }, [screenshotsContainer])
+
     return (
         <div className="relative z-10 pt-7 px-5">
             <span className="float-right text-white text-sm font-rubik customMarginRight spanAfter">Screenshots</span>
-            <div className="clear-right pt-5 overflow-x-hidden whitespace-nowrap cur">
+            <div ref={screenshotsContainer} className="clear-right pt-5 overflow-x-hidden whitespace-nowrap cur">
                 {
                     images.map((image, i) => <img className="w-11/12 inline-block mr-3 last:mr-0" key={i} src={image} alt={`screenshot-${i + 1}`} />)
                 }
             </div>
             <div className="w-9/12 pr-2.5 pt-2">
-                <input className="customScrollbar" type="range" min="0" max="" value="0" step="1"/>
+                <input className="customScrollbar" type="range" min="0" max={scrollWidth} step="1" onInput={(e) => scroll(e)}/>
             </div>
         </div>
     )
